a9a6a10a9d86a65480b1f68fb3dedc2d77af6a50
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-05-25 16:10:51 -0400

    Added isFav value to book objects
    Added isFav property to book objects and enabled different style rules depending on whether the isFav attribute was true or false.

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ const app = Vue.createApp({
             y: 0,
 
             books: [
-                { title: 'Name of the Wind', author: 'Patrick Rothfuss', img: 'assets/1.jpg' },
-                { title: 'The Way of Kings', author: 'Brandon Sanderson', img: 'assets/2.jpg' },
-                { title: 'The Final Empire', author: 'Brandon Sanderson', img: 'assets/3.jpg' }
+                { title: 'Name of the Wind', author: 'Patrick Rothfuss', img: 'assets/1.jpg', isFav: true },
+                { title: 'The Way of Kings', author: 'Brandon Sanderson', img: 'assets/2.jpg', isFav: false },
+                { title: 'The Final Empire', author: 'Brandon Sanderson', img: 'assets/3.jpg', isFav: true }
             ],
 
             url: 'http://www.thenetninja.co.uk',
