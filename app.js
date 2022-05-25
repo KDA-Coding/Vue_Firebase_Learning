e301adb3435a459ab7204ca85a8ae16bd89e6180
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-05-25 15:25:53 -0400

    Added Image assets and used Data Binding
    Introduced to Data Binding with different kinds of data/references.
    
    using v-bind:<attribute> or :<attribute> allows an attribute to be read as a data property through Vue.

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ const app = Vue.createApp({
             author: 'Brandon Sanderson',
             age: 45,
             x: 0,
-            y: 0
+            y: 0,
+
+            books: [
+                { title: 'Name of the Wind', author: 'Patrick Rothfuss', img: 'assets/1.jpg' },
+                { title: 'The Way of Kings', author: 'Brandon Sanderson', img: 'assets/2.jpg' },
+                { title: 'The Final Empire', author: 'Brandon Sanderson', img: 'assets/3.jpg' }
+            ],
+
+            url: 'http://www.thenetninja.co.uk',
+
         }
     },
 
