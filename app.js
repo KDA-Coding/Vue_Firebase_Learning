82a9b5f897965f3b2e4a22b760be8fae02de3535
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-05-25 11:26:00 -0400

    Applied v-on:click methods to elements
    Used the v-on:click (@) directive to make elements respond to click events in different ways.
    
    Some with increments or decrements of numeric values and another with a referenced function that takes an input.

---
+++ app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+    
+    data() {
+        return{
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45
+        }
+    },
+
+    methods: {
+        changeTitle(title) {
+            this.title= title
+        }
+    }
+
+})
+
+app.mount('#app');
