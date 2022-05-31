44f679b88f770a2de1b9549ea28b91352bf7c5ff
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-05-31 15:01:51 -0400

    Added components, click events, props and click event modifiers to project

--- Ch3/modal-project/src/App.vue
+++ Ch3/modal-project/src/App.vue
@@ -1,23 +1,42 @@
 <template>
   <h1>{{ title }}</h1>
-  <input type="text" ref="name">
-  <button @click="handleClick">Click Me</button>
+  <p> Welcome... </p>
+    <div v-if="showModal">
+      <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/>
+    </div>
+  <button @click="toggleModal">Show Modal</button>
 </template>
 
 <script>
+
+import Modal from './components/ModalComponent.vue'
+
 export default {
   name: 'App',
+
+  components: {
+    Modal,
+  },
+
   data() {
     return {
-      title: 'My first Vue App :)'
+      title: 'My first Vue App :)',
+      header: 'Sign up for the Giveaway!',
+      text: 'Grab your Ninja Swag at Half Price!',
+      showModal: false,
     }
   },
 
   methods: {
+    
     handleClick() {
       console.log(this.$refs.name);
       this.$refs.name.classList.add('active')
       this.$refs.name.focus()
+    },
+
+    toggleModal() {
+      this.showModal = !this.showModal;
     }
   }
 }
