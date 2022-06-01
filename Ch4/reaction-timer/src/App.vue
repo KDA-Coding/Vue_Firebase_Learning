16ba7dbc771aceb7e5cf95d3e2c8fa6f40f68b9b
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-06-01 12:26:46 -0400

    Created Endgame event that emits data needed by App
    Used a custom event with $emit to send the results of the timer to app for use in the Block Component and apps Start/End game functions

--- Ch4/reaction-timer/src/App.vue
+++ Ch4/reaction-timer/src/App.vue
@@ -2,7 +2,9 @@
 <h1>Ninja Reaction Timer</h1>
 <button @click="start" :disabled="isPlaying">Play</button>
 
-<Block v-if="isPlaying" :delay="delay"/>
+<Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+
+<p v-if="showResults">Reaction Time: {{ score }} ms </p>
 </template>
 
 <script>
@@ -21,17 +23,25 @@ export default {
     return {
       isPlaying: false,
       delay: null,
-
+      score: null,
+      showResults: false,
     }
   },
 
   methods: {
     
     start() {
+      this.showResults = false;
       this.delay = 2000 + Math.random() * 5000;
       this.isPlaying = true;
       //console.log(this.delay);
     },
+
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.showResults = true
+      this.isPlaying = false
+    }
 
   }
 }
