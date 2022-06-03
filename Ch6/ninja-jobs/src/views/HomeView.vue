5558100ed3be081cc98f872bf8f2610f2477da2b
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-06-03 10:37:58 -0400

    Fixed Paragraph Alignment

--- Ch6/ninja-jobs/src/views/HomeView.vue
+++ Ch6/ninja-jobs/src/views/HomeView.vue
@@ -1,17 +1,8 @@
 <template>
   <div class="home">
     <h1>Home Page</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-       Facere fugit laboriosam consequuntur fuga debitis quae, voluptas maxime esse, 
-       ipsa cum id voluptates ex, obcaecati soluta tenetur exercitationem rem illum? 
-       Doloribus assumenda molestias animi nam earum quidem vitae officia, 
-       impedit sapiente alias numquam aliquam accusantium voluptates nihil tempora dolorem, 
-       voluptatem odio laborum eum facilis optio saepe vero. Facere, perferendis deserunt? 
-       Deleniti inventore possimus ipsa ipsam ratione laudantium accusamus iste 
-       exercitationem dolorem iusto! 
-       Deserunt veniam dolore saepe iusto suscipit in animi deleniti, 
-       unde modi accusantium perferendis temporibus quas, itaque magnam nemo blanditiis 
-       repellendus molestiae. Nihil nostrum rem alias possimus repudiandae? Nihil, optio!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere fugit laboriosam consequuntur fuga debitis quae, voluptas maxime esse, ipsa cum id voluptates ex, obcaecati soluta tenetur exercitationem rem illum? Doloribus assumenda molestias animi nam earum quidem vitae officia, impedit sapiente alias numquam aliquam accusantium voluptates nihil tempora dolorem, voluptatem odio laborum eum facilis optio saepe vero. Facere, perferendis deserunt? Deleniti inventore possimus ipsa ipsam ratione laudantium accusamus iste exercitationem dolorem iusto! Deserunt veniam dolore saepe iusto suscipit in animi deleniti, unde modi accusantium perferendis temporibus quas, itaque magnam nemo blanditiis repellendus molestiae. Nihil nostrum rem alias possimus repudiandae? Nihil, optio!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere fugit laboriosam consequuntur fuga debitis quae, voluptas maxime esse, ipsa cum id voluptates ex, obcaecati soluta tenetur exercitationem rem illum? Doloribus assumenda molestias animi nam earum quidem vitae officia, impedit sapiente alias numquam aliquam accusantium voluptates nihil tempora dolorem, voluptatem odio laborum eum facilis optio saepe vero. Facere, perferendis deserunt? Deleniti inventore possimus ipsa ipsam ratione laudantium accusamus iste exercitationem dolorem iusto! Deserunt veniam dolore saepe iusto suscipit in animi deleniti, unde modi accusantium perferendis temporibus quas, itaque magnam nemo blanditiis repellendus molestiae. Nihil nostrum rem alias possimus repudiandae? Nihil, optio!</p>
   </div>
 </template>
 
