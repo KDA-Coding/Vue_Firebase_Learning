d9c6b653e38c52666142239dc2aade7c92938bbd
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-06-03 10:36:33 -0400

    Initial Config and Commit for Vue Router project

---
+++ Ch6/ninja-jobs/src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About Page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit labore tempora, maxime aliquid aperiam et beatae culpa nulla quaerat accusamus possimus necessitatibus delectus cum eum quasi quos veritatis obcaecati, rem eaque, ea animi dolorem reprehenderit. Necessitatibus consequuntur reiciendis nostrum praesentium tempore. Suscipit esse rem, natus sit sint accusantium sunt odit doloribus? Voluptate ab consectetur sint earum perferendis nulla cupiditate iusto labore, veniam, excepturi quasi incidunt? Ducimus eligendi sapiente eius, officia possimus in, ipsam magni a maiores illum pariatur modi aliquam eum fuga odio optio, iure unde omnis dolorum? Deleniti veritatis dolor omnis numquam explicabo cumque hic sunt quos nisi minus.</p>
+  </div>
+</template>
