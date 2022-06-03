a39b98d0cc74ea3f211cd0a51b3166f546034b08
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-06-03 14:22:18 -0400

    Copied ninja-jobs for JSON Server Practice

---
+++ Ch7/ninja-jobs/src/router/index.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+// import AboutView from '../views/AboutView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
+import JobsView from '../views/jobs/JobsView.vue'
+import JobDetails from '../views/jobs/JobDetails.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: JobsView
+  },
+  {
+    path:'/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetails
+  },
+  //Redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  //Catch-All 404
+  {
+    path:'/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFoundView
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ Ch7/ninja-jobs/src/views/jobs/JobDetails.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Job Details Page</h1>
+  <p> The Job ID is: {{ id }}</p>
+</template>
+
+<script>
+export default {
+    
+    props: [
+        'id',
+    
+    ],
+
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
