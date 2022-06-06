f777c0e57e5e161a766d71f5fa5ebf561973aa52
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-06-06 09:38:08 -0400

    Initial Commit for Project Planner

---
+++ Ch8/project-planner/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #2c3e50;
+}
+
+body {
+  background: #f2f2f2;
+}
+</style>
