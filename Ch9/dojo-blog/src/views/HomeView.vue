da1d0a1543ae6851d56bd3bad291fa715dfebf9f
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-06-07 13:21:12 -0400

    Initial Commit

---
+++ Ch9/dojo-blog/src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <p ref="p" >My name is {{ name }} and I am {{ age }}</p>
+  <button @click="handleClick">Click Me</button>
+</template>
+
+<script>
+
+  import { ref } from 'vue'
+
+export default {
+
+  name: 'HomeView',
+  
+  setup() {
+
+    console.log(this)
+
+    const p = ref(null);
+
+    let name = "mario";
+    let age = 30;
+
+    const handleClick = () => {
+      console.log(p, p.value)
+      p.value.classList.add('test')
+    }
+
+    return { name, age, handleClick, p }
+
+  },
+}
+</script>
