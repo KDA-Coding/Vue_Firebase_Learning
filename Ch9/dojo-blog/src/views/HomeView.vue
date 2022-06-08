80ecd0edaedcea663d9babc15b4c28f3145e918b
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-06-08 14:10:19 -0400

    Used Computed Property in Composition API
    Imported Composition and created computed property that filtered a list based on an input field with .includes.

--- Ch9/dojo-blog/src/views/HomeView.vue
+++ Ch9/dojo-blog/src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <template>
   <h1> Home </h1>
-  <h2> Refs </h2>
-  <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-  <button @click="updateNinjaOne">Update Ninja One</button>
-  
-  <h2> Reactive </h2>
-  <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }} - {{ nameTwo}}</p>
-  <button @click="updateNinjaTwo">Update Ninja Two</button>
+
+  <input type="text" v-model="search">
+  <p> Search Term - {{ search }}</p>
+  <div v-for="name in matchingNames" :key="name">
+    {{ name }}
+  </div>
+
 </template>
 
 <script>
 
-  import { ref, reactive } from 'vue'
+  import { ref, reactive, computed } from 'vue'
 
 export default {
 
@@ -19,25 +19,14 @@ export default {
   
   setup() {
 
-    //const p = ref(null);
+    const search = ref('')
+    const names = ref(['mario','luigi','yoshi','bowser','toad','koopa','peach'])
 
-    const ninjaOne = ref({ name: 'Mario', age: 30})
-    const ninjaTwo = reactive({ name: 'Luigi', age: 25});
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value) )
+    })
 
-    const nameOne = ref('mario')
-    const nameTwo = reactive('luigi')
-
-
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 35
-    }
-
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 30
-      nameTwo = reactive('mario')
-    }
-
-    return { ninjaOne, ninjaTwo, updateNinjaOne, updateNinjaTwo, nameTwo }
+    return { names, search, matchingNames }
 
   },
 }
