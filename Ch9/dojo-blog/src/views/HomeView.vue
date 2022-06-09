ba04fd87e1281e9a40c77fcd9d3f40083ae9129a
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-06-09 15:29:36 -0400

    Cleaned up project for Ch10
    Cleaned up Project for Ch10 involving Async and Await usage.

--- Ch9/dojo-blog/src/views/HomeView.vue
+++ Ch9/dojo-blog/src/views/HomeView.vue
@@ -2,10 +2,7 @@
   <div class="home">
     <h1> Home </h1>
 
-    <PostList v-if="showPosts" :posts="posts"/>
-
-    <button @click="showPosts = !showPosts">Toggle Posts</button>
-    <button @click="posts.pop()">Delete a Post</button>
+    <PostList :posts="posts"/>
 
   </div>
 
@@ -20,21 +17,15 @@ export default {
     
     name: "HomeView",
     
+    components: { PostList },
+
     setup() {
         const posts = ref([
             { title: "Welcome to the Blog", body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam hic ratione aut minus neque eligendi, ipsum odit quo non corporis, repellat sunt aliquam? Molestias, placeat ipsam. Soluta enim pariatur reprehenderit.', id: 1 },
             { title: "Top 5 CSS Tips", body: "Lorem Ipsum", id: 2 }
         ])
 
-        const showPosts = ref(true)
-
-        return { posts, showPosts };
+        return { posts };
     },
-
-    mounted() {
-      console.log('mounted using options api')
-    },
-
-    components: { PostList }
 }
 </script>
