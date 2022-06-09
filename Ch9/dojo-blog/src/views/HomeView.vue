21128afdbbeec2eab0a240e57e8fa800b7ba745c
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-06-09 08:59:36 -0400

    Used Lifecycle Hooks in Setup
    Importing the On[Hook] methods to use in setup allows you to create an arrow function and execute code when the specified lifecycle hook of the component occurs.

--- Ch9/dojo-blog/src/views/HomeView.vue
+++ Ch9/dojo-blog/src/views/HomeView.vue
@@ -2,7 +2,10 @@
   <div class="home">
     <h1> Home </h1>
 
-    <PostList :posts="posts"/>
+    <PostList v-if="showPosts" :posts="posts"/>
+
+    <button @click="showPosts = !showPosts">Toggle Posts</button>
+    <button @click="posts.pop()">Delete a Post</button>
 
   </div>
 
@@ -21,8 +24,15 @@ export default {
         const posts = ref([
             { title: "Welcome to the Blog", body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam hic ratione aut minus neque eligendi, ipsum odit quo non corporis, repellat sunt aliquam? Molestias, placeat ipsam. Soluta enim pariatur reprehenderit.', id: 1 },
             { title: "Top 5 CSS Tips", body: "Lorem Ipsum", id: 2 }
-        ]);
-        return { posts };
+        ])
+
+        const showPosts = ref(true)
+
+        return { posts, showPosts };
+    },
+
+    mounted() {
+      console.log('mounted using options api')
     },
 
     components: { PostList }
