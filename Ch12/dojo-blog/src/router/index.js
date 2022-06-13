0056b15c6bfd9773503ce2f22c105c0d6222ae2e
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-06-13 11:12:38 -0400

    Initial Commit
    Initial Commit for Vue/Firebase project

---
+++ Ch12/dojo-blog/src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import DetailsView from '../views/DetailsView.vue'
+import CreateView from '../views/CreateView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+
+  {
+    path: '/posts/:id',
+    name: 'details',
+    component: DetailsView,
+    props: true
+  },
+
+  {
+    path: '/create',
+    name: 'create',
+    component: CreateView
+  }
+
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
