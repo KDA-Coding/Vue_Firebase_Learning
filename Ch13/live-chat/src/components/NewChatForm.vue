2143116db20289fe4ae965d4db9ac342147364bc
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-07-05 15:39:47 -0400

    Working on Chatroom window and form
    Working on chatroom window, chat data storage and chat text area form for typing and submitting messages to firebase.

--- Ch13/live-chat/src/components/NewChatForm.vue
+++ Ch13/live-chat/src/components/NewChatForm.vue
@@ -1,9 +1,23 @@
 <template>
-  
+  <form>
+    <textarea
+        placeholder="Type a message and hit enter to send."
+        v-model="message"
+    ></textarea>
+  </form>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
 export default {
+
+    setup() {
+        const message = ref('')
+
+        const handleSubmit = async () => {
+            const chat = '';
+        }
+    }
 
 }
 </script>
