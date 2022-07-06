a2fbb242e0d12e9b79a92d11cc54b6b547aafea2
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-07-06 12:49:37 -0400

    Chat form submission works as intended
    Chat form submission works as intended. Creating useCollection composable to store messages in a firestore document.

--- Ch13/live-chat/src/views/ChatRoom.vue
+++ Ch13/live-chat/src/views/ChatRoom.vue
@@ -6,11 +6,6 @@
 </template>
 
 <script>
-// Get the current User using the getUser composable
-// Watch the current user for changes
-// redirect the user to the Welcome page when they are logged out
-
-
 import NavBarVue from '@/components/NavBar.vue'
 import NewChatFormVue from '@/components/NewChatForm.vue'
 import getUser from '@/composables/getUser'
