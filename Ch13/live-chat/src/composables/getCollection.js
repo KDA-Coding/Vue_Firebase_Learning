ffcc2d994098eeb101a5d5344472fa6cfcbb7198
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-07-06 13:51:40 -0400

    Working on collection getter to display chat in real time

---
+++ Ch13/live-chat/src/composables/getCollection.js
@@ -0,0 +1,12 @@
+import { ref } from "vue";
+import { projectFirestore } from "@/components/firebase/config";
+
+const getCollection = (collection) => {
+    const documents = ref(null)
+    const error = ref(null)
+    
+
+
+}
+
+export default getCollection
