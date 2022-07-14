14550561c4674285a93fe9f3298542b6cca22569
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2022-07-14 15:24:20 -0400

    Auto-Scrolling Chat Window complete
    Used ref to the main chat window div for auto scrolling when updated (on load and on new messages sent)

--- Ch13/live-chat/src/components/ChatWindow.vue
+++ Ch13/live-chat/src/components/ChatWindow.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="chat-window">
     <div v-if="error">{{ error }}</div>
-    <div v-if="documents" class="messages">
-        <div v-for="doc in documents" :key="doc.id" class="single">
-            <span class="created-at">{{ doc.createdAt.toDate() }}</span>
+    <div v-if="documents" class="messages" ref="messages">
+        <div v-for="doc in formattedDocuments" :key="doc.id" class="single">
+            <span class="created-at">{{ doc.createdAt }}</span>
             <span class="name">{{ doc.name }}</span>
             <span class="message">{{ doc.message }}</span>
         </div>
@@ -14,13 +14,32 @@
 <script>
 
 import getCollection from '@/composables/getCollection'
+import { formatDistanceToNow } from 'date-fns'
+import { computed, onUpdated, ref } from '@vue/runtime-core'
+
 
 export default {
 
     setup() {
         const { error, documents } = getCollection('messages')
 
-        return { error, documents }
+        const formattedDocuments = computed(() => {
+            if (documents.value) {
+                return documents.value.map(doc => {
+                    let time = formatDistanceToNow(doc.createdAt.toDate())
+                    return { ...doc, createdAt: time }
+                })
+            }
+        })
+
+        // Auto-scroll to bottom of messages
+        const messages = ref(null);
+
+        onUpdated(() => {
+            messages.value.scrollTop = messages.value.scrollHeight
+        })
+
+        return { messages, error, documents, formattedDocuments }
     }
 
 }
